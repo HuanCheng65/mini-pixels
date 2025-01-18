63e5af1b8f4a80450f39ede211634e39a7d50b11
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2025-01-18 20:46:42 +0800

    feat(cli): use readline for better input handling

--- pixels-cli/main.cpp
+++ pixels-cli/main.cpp
@@ -22,29 +22,34 @@
 // Created by gengdy on 11/16/24.
 //
 
-#include <iostream>
-#include <sstream>
-#include <string>
 #include <boost/algorithm/string.hpp>
 #include <boost/program_options.hpp>
 #include <executor/LoadExecutor.h>
+#include <iostream>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <sstream>
+#include <string>
 
 namespace bpo = boost::program_options;
 
-int main () {
+int main() {
     std::string inputStr;
 
     while (true) {
-        std::cout << "pixels> ";
-        if (!std::getline(std::cin, inputStr)) {
+        char *line = readline("pixels> ");
+        add_history(line);
+        if (!line) {
             // in case of input from a file, exit at EOF
             std::cout << "Bye." << std::endl;
             break;
         }
+        inputStr = line;
+        free(line);
         boost::trim(inputStr);
 
         if (inputStr.empty() || inputStr == ";") {
-                    continue;
+            continue;
         }
 
         if (inputStr.back() == ';') {
@@ -53,30 +58,34 @@ int main () {
 
         // process exit command
         std::string lowerInputStr = boost::to_lower_copy(inputStr);
-        if (lowerInputStr == "exit" || lowerInputStr == "quit" || lowerInputStr == "-q") {
+        if (lowerInputStr == "exit" || lowerInputStr == "quit" ||
+            lowerInputStr == "-q") {
             std::cout << "Bye." << std::endl;
             break;
         }
 
         // process help command
         if (lowerInputStr == "help" || lowerInputStr == "-h") {
-            std::cout << "Supported commands:\n" <<
-                        "LOAD\n" <<
-                        "COMPACT\n" <<
-                        "IMPORT\n" <<
-                        "STAT\n" <<
-                        "QUERY\n" <<
-                        "COPY\n" <<
-                        "FILE_META\n";
-            std::cout << "{command} -h to show the usage of a command.\nexit / quit / -q to exit.\n";
+            std::cout << "Supported commands:\n"
+                      << "LOAD\n"
+                      << "COMPACT\n"
+                      << "IMPORT\n"
+                      << "STAT\n"
+                      << "QUERY\n"
+                      << "COPU\n"
+                      << "FILE_META";
+            std::cout << "{command} -h to show the usage of a command.\nexit / "
+                         "quit / -q to exit.\n";
             continue;
         }
 
         // Split input into tokens as char* array
         std::istringstream iss(inputStr);
-        std::vector<std::string> token_strings{std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{}};
-        std::vector<char*> argv;
-        for (const auto& str : token_strings) {
+        std::vector<std::string> token_strings{
+            std::istream_iterator<std::string>{iss},
+            std::istream_iterator<std::string>{}};
+        std::vector<char *> argv;
+        for (const auto &str : token_strings) {
             argv.push_back(strdup(str.c_str())); // Duplicate strings as char*
         }
         if (argv.empty()) {
@@ -88,56 +97,57 @@ int main () {
 
         if (command == "LOAD") {
             bpo::options_description desc("Pixels ETL LOAD");
-            desc.add_options()
-                    ("help,h", "show this help message and exit")
-                    ("origin,o", bpo::value<std::string>()->required(), "specify the path of original data files")
-                    ("target,t", bpo::value<std::string>()->required(), "specify the path of target data files")
-                    ("schema,s", bpo::value<std::string>()->required(), "specify the schema of pixels")
-                    ("row_num,n", bpo::value<int>()->required(), "specify the max number of rows to write in a file")
-                    ("row_regex,r", bpo::value<std::string>()->required(), "specify the split regex of each row in a file")
-                    ("encoding_level,e", bpo::value<int>()->default_value(2), "specify the encoding level for data loading")
-                    ("nulls_padding,p", bpo::value<bool>()->default_value(false), "specify whether nulls padding is enabled");
+            desc.add_options()("help,h", "show this help message and exit")(
+                "origin,o", bpo::value<std::string>()->required(),
+                "specify the path of original data files")(
+                "target,t", bpo::value<std::string>()->required(),
+                "specify the path of target data files")(
+                "schema,s", bpo::value<std::string>()->required(),
+                "specify the schema of pixels")(
+                "row_num,n", bpo::value<int>()->required(),
+                "specify the max number of rows to write in a file")(
+                "row_regex,r", bpo::value<std::string>()->required(),
+                "specify the split regex of each row in a file")(
+                "encoding_level,e", bpo::value<int>()->default_value(2),
+                "specify the encoding level for data loading")(
+                "nulls_padding,p", bpo::value<bool>()->default_value(false),
+                "specify whether nulls padding is enabled");
 
             bpo::variables_map vm;
             try {
-                bpo::store(bpo::parse_command_line(argv.size(), argv.data(), desc), vm);
+                bpo::store(
+                    bpo::parse_command_line(argv.size(), argv.data(), desc),
+                    vm);
                 if (vm.count("help")) {
                     std::cout << desc << std::endl;
                     continue;
                 }
                 bpo::notify(vm);
-            } catch (const bpo::error& e) {
-                 std::cerr << "Error parsing options: " << e.what() << "\n";
+            } catch (const bpo::error &e) {
+                std::cerr << "Error parsing options: " << e.what() << "\n";
             }
             LoadExecutor *loadExecutor = new LoadExecutor();
             loadExecutor->execute(vm, command);
 
             // free loadExecutor
             delete loadExecutor;
-        }
-        else if (command == "QUERY") {
+        } else if (command == "QUERY") {
             std::cout << "Not implemented yet." << std::endl;
-        }
-        else if (command == "COPY") {
+        } else if (command == "COPY") {
             std::cout << "Not implemented yet." << std::endl;
-        }
-        else if (command == "COMPACT") {
+        } else if (command == "COMPACT") {
             std::cout << "Not implemented yet." << std::endl;
-        }
-        else if (command == "STAT") {
+        } else if (command == "STAT") {
             std::cout << "Not implemented yet." << std::endl;
-        }
-        else if (command == "IMPORT") {
+        } else if (command == "IMPORT") {
             std::cout << "Not implemented yet." << std::endl;
-        }
-        else if (command == "FILE_META") {
+        } else if (command == "FILE_META") {
             std::cout << "Not implemented yet." << std::endl;
-        }
-        else {
+        } else {
             std::cout << "Command " << command << " not found" << std::endl;
         }
         // free argv
-        for (auto& arg : argv) {
+        for (auto &arg : argv) {
             free(arg);
         }
     } // end of while loop
